684f7a079022c84b67d4b5e1e5cfcced2bcb6d20
Shanna Wallace <shannaw04@yahoo.com>
2024-03-09 15:32:25 -0500

    adjusted grid separation values for 2 row layouts

--- cs_godot_new/bottleScript.gd
+++ cs_godot_new/bottleScript.gd
@@ -3,13 +3,25 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var button = Button.new()
+	button.modulate = Color(0,0,0,0)
+	button.pressed.connect(self._button_pressed)
+	add_child(button)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-
-	
+func _button_pressed():
+	# 1st bottle selected
+	if get_parent().get_parent().bottlesPressed == 0 or get_parent().get_parent().bottlesPressed == 2:
+		print(get_name(), " pressed 1st")
+		get_parent().get_parent().bottlesPressed = 1
+		get_parent().get_parent().bottleFrom = self
+	# 2nd bottle selected
+	elif get_parent().get_parent().bottlesPressed == 1:
+		print(get_name(), " pressed 2nd")
+		get_parent().get_parent().bottlesPressed = 2
+		get_parent().get_parent().bottleTo = self
+		print("makeMove(", get_parent().get_parent().bottleFrom.get_name(), ", ", get_parent().get_parent().bottleTo.get_name(), ")")	
--- cs_godot_new/playGame.gd
+++ cs_godot_new/playGame.gd
@@ -4,9 +4,15 @@ var levels = Array() # each element is an array of data for 1 level
 var colors = Array() # list of rgba values for the blocks. index corresponds to
 					 # the number in the level data file
 
+var emptyColor = Color(0, 0, 0, 1)
 var numColors        # number of colors in the level
 var numBlocks        # number of blocks per bottle in the level
 var numBottles       # number of bottles in the level
+
+var bottlesCompleted 
+var bottlesPressed = 0
+@onready var bottleFrom = get_node("bottles/bottle1")
+@onready var bottleTo = get_node("bottles/bottle1")
 
 var gameFilename = "res://levelData/1per.txt"
 var colorFilename = "res://colors.txt"
@@ -15,7 +21,11 @@ var bottlesScene = preload("res://bottles.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	loadGameData()
+	#bottlesCompleted = 0
+	#loadLevelData(levels[2])
+
 	for level in levels:
+		bottlesCompleted = 0
 		loadLevelData(level)
 		await get_tree().create_timer(2).timeout
 		
@@ -28,6 +38,7 @@ func _ready():
 		bottles.set_position(pos)
 		bottles.set_size(gridSize)
 		add_child(bottles)
+
 		
 
 
@@ -88,6 +99,13 @@ func loadLevelData(level):
 	var cols = level.pop_front()
 	bottlesGrid.columns = cols
 	
+	# adjust bottle separation for 2 rows
+	if rows == 2:
+		bottlesGrid.add_theme_constant_override("h_separation", 100)
+		if cols != 4:
+			bottlesGrid.add_theme_constant_override("v_separation", 200)
+			
+	
 	# get numBottles, numBlocks, and numColors
 	# pop these off so level[0] will be the color val of the first block. 
 	# makes setting the colors easier 
@@ -130,7 +148,6 @@ func loadLevelData(level):
 	# so if level[2] == 1, then the color will be at colors[1].
 	var blockCounter = 0 
 	# iterate through the bottles
-	print(bottlesGrid.get_children())
 	for i in bottlesGrid.get_children():
 		# iterate through the bottle's blocks
 		for j in i.get_child(0).get_children():
@@ -138,11 +155,42 @@ func loadLevelData(level):
 			rgba = colors[level[blockCounter]]
 			blockCounter = blockCounter + 1
 			j.set_color(Color(rgba[0], rgba[1], rgba[2], rgba[3]))
+
+	# set bottle metadata
+	for i in bottlesGrid.get_children():
+		# topColor
+		var topColor = i.get_child(0).get_child(0).get_color()
+		i.set_meta("isComplete", false)
+		i.set_meta("topColor", topColor)
+		
+		# if top color is the empty color, then this bottle is empty
+		if topColor == emptyColor:
+			i.set_meta("emptyBlocks", numBlocks)
+			i.set_meta("topColorBlocks", 0)
+		# count how many consecutive blocks of top color there are
+		else:
+			var topColorBlocks = 0
+			for j in i.get_child(0).get_children():
+				if j.get_color() == topColor:
+					topColorBlocks = topColorBlocks + 1
+				else:
+					break
+			i.set_meta("topColorBlocks", topColorBlocks)
+			if topColorBlocks == numBlocks:
+				i.set_meta("isComplete", true)
+				bottlesCompleted = bottlesCompleted + 1
+			else:
+				i.set_meta("isComplete", false)
+				
+	# check the meta data
+	for i in bottlesGrid.get_children():
+		print(i.get_name(), ":  color = ", i.get_meta("topColor"), "  top color blocks = ", i.get_meta("topColorBlocks"), "  number of empty blocks: ", i.get_meta("emptyBlocks"))
 	
 	return true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# while bottlesCompleted != numColors:
 	pass
 
